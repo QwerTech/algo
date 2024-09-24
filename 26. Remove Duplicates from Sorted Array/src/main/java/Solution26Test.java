31dade848ced3047682f7cacc90c03104d145732
Pavel Novikov <novikopa@amazon.com>
2024-09-23 23:23:45 -0400

    implement 26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/src/main/java/Solution26Test.java
@@ -0,0 +1,38 @@
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class Solution26Test {
+
+    public static Stream<Arguments> cases() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 1, 2}, new int[]{1, 2}),
+                Arguments.of(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, new int[]{0, 1, 2, 3, 4})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    void test(int[] nums, int[] expectedNums) {
+        int k = removeDuplicates(nums);
+
+        Assertions.assertEquals(expectedNums.length, k);
+        int[] dest = new int[k];
+        System.arraycopy(nums, 0, dest, 0, k);
+        Assertions.assertArrayEquals(expectedNums, dest);
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k]) {
+                k++;
+                nums[k] = nums[i];
+            }
+        }
+        return k + 1;
+    }
+}
