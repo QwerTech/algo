abe55623fe1ac6f9c6175ec7208389dc15abdbe7
Pavel Novikov <novikopa@amazon.com>
2024-09-26 00:19:55 -0400

    implement 189. Rotate Array

---
+++ 189. Rotate Array/src/main/java/Solution189Test.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class Solution189Test {
+    public static Stream<Arguments> cases() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4, 5, 6, 7}, 3, new int[]{5, 6, 7, 1, 2, 3, 4}),
+                Arguments.of(new int[]{1, 2, 3, 4, 5, 6, 7}, 2, new int[]{6, 7, 1, 2, 3, 4, 5}),
+                Arguments.of(new int[]{-1, -100, 3, 99}, 2, new int[]{3, 99, -1, -100})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    void test(int[] nums, int k, int[] expectedNums) {
+        rotate(nums, k);
+        System.out.println(Arrays.toString(nums));
+        Assertions.assertArrayEquals(expectedNums, nums);
+    }
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
